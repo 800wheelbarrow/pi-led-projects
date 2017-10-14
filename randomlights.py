5764bafe458c6130aa7baeeb2e53e0993c086f5f
Pi Zero <31018771+800wheelbarrow@users.noreply.github.com>
2017-10-14 16:00:03 -0400

    Add new function to run all other functions

--- randomlights.py
+++ randomlights.py
@@ -2,14 +2,20 @@ import time
 import random
 import unicornhat as uh
 
-uh.set_layout(uh.PHAT)
-uh.brightness(0.7)
+def randomLights():
 
-for x in range(8):
-	for y in range(4):
-		a = random.randint(0, 255)
-		b = random.randint(0, 255)
-		c = random.randint(0, 255)
-        	uh.set_pixel(x, y, a, b, c)
-uh.show()
-time.sleep(5)
+	uh.set_layout(uh.PHAT)
+	uh.brightness(0.7)
+
+	for x in range(8):
+		for y in range(4):
+			a = random.randint(0, 255)
+			b = random.randint(0, 255)
+			c = random.randint(0, 255)
+	        	uh.set_pixel(x, y, a, b, c)
+	uh.show()
+	time.sleep(5)
+	uh.clear()
+
+if __name__ == '__main__':
+        randomLights()
