155ffe551c4326c25f21d620d02ee564592c0528
Pi Zero <31018771+800wheelbarrow@users.noreply.github.com>
2017-11-06 22:24:36 -0500

    add weather script to web server

--- webserver.py
+++ webserver.py
@@ -1,11 +1,12 @@
 import randomlights
 import timeflash
+import weather2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def landing():
-        return 'This is the Pi Zero web server.'
+        return '<p>This is the Pi Zero web server.</p><p><a href="/random">Random</a></p><p><a href="/time">Time</a></p><p><a href="/weather">Weather</a></p>'
 
 @app.route('/random')
 def random():
@@ -16,3 +17,8 @@ def random():
 def time():
         timeflash.flashAll();
         return 'Time flash complete.'
+
+@app.route('/weather')
+def wea():
+        weather2.weather();
+        return 'Weather flash complete.'
